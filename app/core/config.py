bb443303d54198374eae065eb01a785bd2cfedb7
Madhav Manoj <maadhavmanoj21@gmail.com>
2026-08-02 20:26:57 +0530

    Config.py

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+# Project Root
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+
+# Data Folder
+DATA_DIR = PROJECT_ROOT / "data"
+
+# Storage Directories
+RAW_PDF_DIR = DATA_DIR / "raw_pdfs"
+MARKDOWN_DIR = DATA_DIR / "markdown"
+CLEANED_MARKDOWN_DIR = DATA_DIR / "cleaned_markdown"
+
+# Automatically create directories
+for directory in [
+    RAW_PDF_DIR,
+    MARKDOWN_DIR,
+    CLEANED_MARKDOWN_DIR,
+]:
+    directory.mkdir(parents=True, exist_ok=True)
